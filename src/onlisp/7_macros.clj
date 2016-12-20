3ae7d25778f7c4d8523d46710deeb4bf98d90d56
Eli Bendersky <eliben@gmail.com>
2016-12-20 05:45:59 -0800

    Added our-do from page 98

--- src/onlisp/7_macros.clj
+++ src/onlisp/7_macros.clj
@@ -8,9 +8,9 @@
 
 (def foo 3)
 (mac
-  (our-when (> foo 4)
-          (prn foo)
-          (prn (inc foo))))
+ (our-when (> foo 4)
+           (prn foo)
+           (prn (inc foo))))
 
 ;;; Clojure already has 'while'; this recreates it with a loop. Note that 'when'
 ;;; is also a macro, so a full macro expansion will expand that as well.
@@ -22,9 +22,9 @@
        (recur))))
 
 (mac
-  (our-while hungry
-             (stare-intently)
-             (meow)))
+ (our-while hungry
+            (stare-intently)
+            (meow)))
 
 ;;; Similar to Clojure's when-let
 (defmacro when-bind
@@ -34,9 +34,82 @@
        ~@body)))
 
 (mac
-    (when-bind (inp (get-user-input))
-               (process input)))
+ (when-bind (inp (get-user-input))
+            (process input)))
 
 (def alst '())
 (when-bind (s (seq alst))
            (rest s))
+
+;;; Implementing CL's do macro
+;;;
+;;; (our-do
+;;;      ((w 3)
+;;;       (x 1 (inc x))
+;;;       (y 2 (inc y))
+;;;       (z))
+;;;     ((> x 10) (prn z) y)
+;;;  (prn x)
+;;;  (prn y))
+;;;
+;;; Should expand to:
+;;;
+;;; (loop [w 3
+;;;        x 1
+;;;        y 2
+;;;        z nil]
+;;;   (if (> x 10)
+;;;     (do (prn z) y)
+;;;     (do (prn x) (prn y) (recur w (inc x) (inc y) z))))
+
+(defn make-initforms
+  [bindforms]
+  (mapcat
+   (fn [b]
+     (if (seq b)
+       [(first b) (second b)]
+       [b nil]))
+   bindforms))
+
+(defn make-stepforms
+  [bindforms]
+  (map
+   (fn [b]
+     (if (and (seq b) (> (count b) 2))
+       (nth b 2)
+       (first b)))
+   bindforms))
+
+(make-initforms '((w 3) (x 1 (inc x)) (y 2 (inc y)) (z)))
+(make-stepforms '((w 3) (x 1 (inc x)) (y 2 (inc y)) (z)))
+
+(defmacro our-do
+  [bindforms [test & result] & body]
+  `(loop ~(vec (make-initforms bindforms))
+     (if ~test
+       (do ~@result)
+       (do
+         ~@body
+         (recur ~@(make-stepforms bindforms))))))
+
+;(mac
+ (our-do
+      ((w 3)
+       (x 1 (inc x))
+       (y 2 (inc y))
+       (z))
+     ((> x 10) (prn z) y)
+  (prn x)
+  (prn y))
+;)
+
+;(loop [w 3
+      ;x 1
+      ;y 2
+      ;z nil]
+ ;(if (> x 10)
+   ;(do (prn z) y)
+   ;(do
+     ;(prn x)
+     ;(prn y)
+     ;(recur w (inc x) (inc y) z))))

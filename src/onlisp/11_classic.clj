aec7311fa8c729e344c978be2341713c3c560319
Eli Bendersky <eliben@gmail.com>
2016-12-26 06:54:30 -0800

    Adding more macros from chapter 11

--- src/onlisp/11_classic.clj
+++ src/onlisp/11_classic.clj
@@ -23,3 +23,54 @@
 (with-gensyms (gob x0 y1)
   (list gob x0 y1))
 )
+
+(defmacro nif
+  [expr pos zero neg]
+  (let [g (gensym)]
+    `(let [~g ~expr]
+       (cond
+         (> ~g 0) ~pos
+         (= ~g 0) ~zero
+         :else ~neg))))
+
+(map #(nif % 'p 'z 'n) '(1 -1 0 -2 7))
+
+;;; Generates efficient code to check whether obj is one of the rest of the
+;;; arguments.
+(defmacro in
+  [obj & choices]
+  (let [insym (gensym)]
+    `(let [~insym ~obj]
+       (or ~@(map (fn [c] `(= ~insym ~c)) choices)))))
+
+(mac
+(in 'k 'a 'b 'c 'k 'd)
+)
+
+;;; Helper for 'in' that quotes arguments
+(defmacro inq
+  [obj & choices]
+  `(in ~obj ~@(map (fn [c] `'~c) choices)))
+
+(mac
+(inq 'k a b c d k d)
+)
+
+(defn pairs
+  [lst]
+  (mapv #(take 2 (nthrest lst %)) (range 0 (dec (count lst)))))
+
+;;; Equivalent of do-tuples/o on page 156. The name is Clojure-ized since / is
+;;; for namespace separation.
+(defmacro do-tuples-o
+  [parms source & body]
+  (if parms
+    (let [src (gensym)]
+      `(let [~src ~source]
+         (doseq 
+           [~parms (pairs ~src)]
+           ~@body)))))
+
+(mac
+(do-tuples-o [x y] '(1 2 30 4 5) (prn (+ x y)))
+)
